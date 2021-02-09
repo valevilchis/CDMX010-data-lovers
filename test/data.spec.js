070d58bc31d38741e8d405a355d85095d6037137
Valeri11-Vil <vilchisvaleria11@gmail.com>
2021-02-09 15:06:09 -0600

    Data Lover con Jest/Async

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,34 @@
-import { example, anotherExample } from '../src/data.js';
+// import { example, anotherExample } from '../src/data.js';
 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
+// describe('example', () => {
+//   it('is a function', () => {
+//     expect(typeof example).toBe('function');
+//   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
+//   it('returns `example`', () => {
+//     expect(example()).toBe('example');
+//   });
+// });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
+// describe('anotherExample', () => {
+//   it('is a function', () => {
+//     expect(typeof anotherExample).toBe('function');
+//   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
+//   it('returns `anotherExample`', () => {
+//     expect(anotherExample()).toBe('OMG');
+//   });
+// });
+
+import {getPokemones} from '../src/data.js';
+
+describe('Probar Async/Await', () => {
+
+    it('Realizar peticion a una api', async () => {
+        await getPokemones().then(data => {
+            expect(data.results).toBeGreaterThan[0];
+        });
+    });
 });
